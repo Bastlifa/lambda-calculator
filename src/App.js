5d118eb5414bb085167a1f25da8e0859f64eceb9
Eli Sacks <bastlifa@gmail.com>
2019-07-16 17:12:24 -0400

    er, MVP complete, now with tracked files

--- src/App.js
+++ src/App.js
@@ -1,8 +1,13 @@
 import React from "react";
-import "./App.css";
+
+
+import "./App.scss";
 // STEP 4 - import the button and display components
 // Don't forget to import any extra css/scss files you build into the correct component
-
+import Numbers from './components/ButtonComponents/NumberButtons/Numbers'
+import Operators from './components/ButtonComponents/OperatorButtons/Operators'
+import Specials from './components/ButtonComponents/SpecialButtons/Specials'
+import Display from './components/DisplayComponents/Display'
 // Logo has already been provided for you. Do the same for the remaining components
 import Logo from "./components/DisplayComponents/Logo";
 
@@ -18,6 +23,14 @@ function App() {
       <Logo />
       <div className="App">
         {/* STEP 4 - Render your components here and be sure to properly import/export all files */}
+        <Display/>
+        <div className={"all-buttons"}>
+          <div className={`left-col-buttons`}>
+            <Specials/>
+            <Numbers />
+          </div>
+          <Operators />
+        </div>
       </div>
     </div>
   );
--- src/components/DisplayComponents/Display.js
+++ src/components/DisplayComponents/Display.js
@@ -1,5 +1,8 @@
 import React from "react";
 
+// import './display.css'
 const Display = () => {
-  return <div>{/* Display any props data here */}</div>;
+  return <div className={`display`}>{/* Display any props data here */}</div>;
 };
+
+export default Display
