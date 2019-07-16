ff29c84ac01bfe6f7f50a9d7e1cec33b06b228b7
Eli Sacks <bastlifa@gmail.com>
2019-07-16 17:00:34 -0400

    oops, nearly done with MVP...

--- src/components/ButtonComponents/NumberButtons/NumberButton.js
+++ src/components/ButtonComponents/NumberButtons/NumberButton.js
@@ -1,9 +1,13 @@
 import React from "react";
 
-const NumberButton = () => {
+const NumberButton = (props) => {
+  const {number} = props;
   return (
     <>
       {/* Display a button element rendering the data being passed down from the parent container on props */}
+      <button className={number === "0" ? `button number-button zero-button` : `button number-button`}>{number}</button>
     </>
   );
 };
+
+export default NumberButton
